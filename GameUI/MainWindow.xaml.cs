81091d1bc388a26b475154e8b35aef25550ee4cf
Faig Ibragimov <149930150+wdsunsif@users.noreply.github.com>
2024-10-14 19:21:00 +0400

    Add files via upload

---
+++ GameUI/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Text.Json;
+
+namespace GameUI;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+		DataContext = this;
+
+		IsX = true;
+		IsStart = false;
+
+		endPoint = new IPEndPoint(ip, port);
+	}
+
+
+	TcpClient client = new TcpClient();
+
+	IPAddress ip = IPAddress.Parse("127.0.0.1");
+	int port = 27001;
+
+	IPEndPoint endPoint;
+
+	public bool IsX { get; set; }
+	public bool IsStart { get; set; }
+
+	private void ButtonClick(object sender, RoutedEventArgs e)
+	{
+		if (!IsStart) return;
+
+		var B = sender as Button;
+
+		//	if (IsX) B.Content = "X";
+		//else B.Content = chr;
+
+		//	IsX = !IsX;
+		B.IsEnabled = false;
+
+
+		byte currIndex = (byte)(Convert.ToByte(B.Name.Last()) - 49);
+
+		try
+		{
+			client.Connect(endPoint);
+
+			if (client.Connected)
+			{
+				var writer = Task.Run(() =>
+				{
+					while (true)
+					{
+						var stream = client.GetStream();
+						var bw = new BinaryWriter(stream);
+
+						bw.Write(JsonSerializer.Serialize(currIndex));
+					}
+				});
+
+				var reader = Task.Run(() =>
+				{
+					while (true)
+					{
+						var stream = client.GetStream();
+						var br = new BinaryReader(stream);
+
+						var chr = br.ReadString();
+						
+						switch (currIndex)
+						{
+							case 0:
+								b1.Content = chr;
+								break;
+							case 1:
+								b2.Content = chr;
+								break;
+							case 2:
+								b3.Content = chr;
+								break;
+							case 3:
+								b4.Content = chr;
+								break;
+							case 4:
+								b5.Content = chr;
+								break;
+							case 5:
+								b6.Content = chr;
+								break;
+							case 6:
+								b7.Content = chr;
+								break;
+							case 7:
+								b8.Content = chr;
+								break;
+							case 8:
+								b9.Content = chr;
+								break;
+
+						}
+					}
+				});
+
+				Task.WaitAll(writer, reader);
+			}
+			else
+			{
+				Console.WriteLine("NOT Connected.\n");
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Exception occoured.\n\t {ex.Message}\n");
+		}
+
+
+
+
+	}
+
+	private void ReadyBtn_Click(object sender, RoutedEventArgs e)
+	{
+		IsStart = true;
+	}
+}
---
+++ Maingame/Program.cs
@@ -0,0 +1,121 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+TcpListener listener = null;
+BinaryReader br = null;
+BinaryWriter bw = null;
+List<TcpClient> clients = [];
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+var endPoint = new IPEndPoint(ip, port);
+listener = new TcpListener(endPoint);
+listener.Start();
+Console.WriteLine($"Listening on {listener.LocalEndpoint}.");
+
+bool isX = true;
+byte[] arr = new byte[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+while (true)
+{
+	var client = listener.AcceptTcpClient();
+	clients.Add(client);
+
+	Console.WriteLine($"{client.Client.RemoteEndPoint} connected...");
+
+	var reader = Task.Run(() =>
+	{
+		foreach (var client in clients)
+		{
+			Task.Run(() =>
+			{
+				while (true)
+				{
+					var stream = client.GetStream();
+					br = new BinaryReader(stream);
+
+					try
+					{
+						var msg = br.ReadString();
+
+						byte indx = JsonSerializer.Deserialize<byte>(msg);
+
+						if (isX)
+						{
+							arr[indx] = 1;
+						}
+						else
+						{
+							arr[indx] = 2;
+						}
+						isX = !isX;
+
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine($"{client.Client.RemoteEndPoint} removed.");
+						clients.Remove(client);
+					}
+				}
+			}).Wait(50);
+		}
+	});
+
+	var writer = Task.Run(() =>
+	{
+		while (true)
+		{
+			foreach (var client in clients)
+			{
+				var stream = client.GetStream();
+				bw = new BinaryWriter(stream);
+				var msg = "";
+
+				if (isX)
+				{
+					msg = "X";
+				}
+				else
+				{
+					msg = "O";
+				}
+				var cubicSize = 3;
+				for (int i = 0; i < cubicSize; i++)
+				{
+					if ((arr[i * cubicSize] == arr[i * cubicSize + 1]) &&
+						(arr[i * cubicSize] == arr[i * cubicSize + 2]) &&
+						(arr[i * cubicSize] != ' '))
+					{
+						msg = arr[i * cubicSize] == 1 ? "X" : "O";
+						break;
+					}
+				}
+
+				for (int i = 0; i < cubicSize; i++)
+				{
+					if ((arr[i] == arr[i + 3]) &&
+						(arr[i] == arr[i + 6]) &&
+						(arr[i] != ' '))
+					{
+						msg = arr[i] == 1 ? "X" : "O";
+						break;
+					}
+				}
+
+				if (((arr[0] == arr[4]) && (arr[0] == arr[8]) && (arr[0] != ' ')) ||
+					((arr[2] == arr[4]) && (arr[2] == arr[6]) && (arr[2] != ' ')))
+				{
+					msg = arr[4] == 1 ? "X" : "O";
+					break;
+				}
+
+				bw.Write(msg);
+			}
+		}
+	});
+
+	Task.WaitAll(reader, writer);
+}
